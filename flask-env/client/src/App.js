d13558b44dcdc9dfa39b47eae560632cb269c1e2
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-06-06 12:31:20 -0500

    Fixed Nav issues

--- flask-env/client/src/App.js
+++ flask-env/client/src/App.js
@@ -1,46 +1,20 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import Footer from "./components/Footer";
 import FileSubmission from "./components/FileSubmission";
+import Nav from './components/Navbar'
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
-      // <h1>Hello</h1>
       <Router>
         <div>
-          <h2>Welcome to SkillRoad</h2>
-          {/*<nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              <li>
-                <Link to={"/"} className="nav-link">
-                  {" "}
-                  Home{" "}
-                </Link>
-              </li>
-              <li>
-                <Link to={"/header"} className="nav-link">
-                  Header
-                </Link>
-              </li>
-              <li>
-                <Link to={"/footer"} className="nav-link">
-                  Footer
-                </Link>
-              </li>
-              <li>
-                <Link to={"/submit"} className="nav-link">
-                  FileSubmission
-                </Link>
-              </li>
-            </ul>
-          </nav>*/}
+          <Header />
           <hr />
           <Switch>
-            <Route path="/header" component={Header} />
             <Route exact path="/" component={LandingPage} />
             <Route path="/submit" component={FileSubmission} />
             <Route path="/footer" component={Footer} />

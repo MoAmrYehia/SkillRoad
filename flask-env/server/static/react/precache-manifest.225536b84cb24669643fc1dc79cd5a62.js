63c532e7080d882315e9fd687b573c5b61dd13e2
gripepi <gian.ripepi96@gmail.com>
2020-06-06 03:44:59 +0100

    FIxed errors

---
+++ flask-env/server/static/react/precache-manifest.225536b84cb24669643fc1dc79cd5a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b920aa60ac09b659e70b51e78121e5f9",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "3c7098680523cbe95798",
+    "url": "/static/react/js/2.419ec58a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/react/js/2.419ec58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2b785b095008916acb",
+    "url": "/static/react/js/main.2c19e9fa.chunk.js"
+  },
+  {
+    "revision": "a0ba83aebf1ca9a3fa74",
+    "url": "/static/react/js/runtime-main.2b7f3823.js"
+  }
+]);

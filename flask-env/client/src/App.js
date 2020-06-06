80f832cec04033ad66d9386dd8a58a428c1529ac
gripepi <gian.ripepi96@gmail.com>
2020-06-06 04:04:22 +0100

    Eject

--- flask-env/client/src/App.js
+++ flask-env/client/src/App.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Header from './components/Header';
-import LandingPage from './components/LandingPage';
-import Footer from './components/Footer';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-    <Router>
-        <div>
-          <h2>Welcome to SkillRoad</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/header'} className="nav-link">Header</Link></li>
-            <li><Link to={'/footer'} className="nav-link">Footer</Link></li>
-          </ul>
-          </nav>
-          <hr />
-          <Switch>
-              <Route exact path='/' component={LandingPage} />
-              <Route path='/header' component={Header} />
-              <Route path='/footer' component={Footer} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;

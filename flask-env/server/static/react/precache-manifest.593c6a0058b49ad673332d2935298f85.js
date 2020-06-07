d35b702241a30bbf77ddd2c8713e5bb5d0b4776c
zejiran <juanszalegria@gmail.com>
2020-06-06 23:28:22 -0500

    upload

---
+++ flask-env/server/static/react/precache-manifest.593c6a0058b49ad673332d2935298f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a8e15c6d30d04e1bb11d439657b200",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "b0d5e06c8616d58333fa",
+    "url": "/static/react/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b0d5e06c8616d58333fa",
+    "url": "/static/react/js/2.d7f34cef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/react/js/2.d7f34cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915c4a28a3805eb23112",
+    "url": "/static/react/js/main.ff2357aa.chunk.js"
+  },
+  {
+    "revision": "a0ba83aebf1ca9a3fa74",
+    "url": "/static/react/js/runtime-main.2b7f3823.js"
+  },
+  {
+    "revision": "1aba88ac201f621bf1cd9acc51de488f",
+    "url": "/static/react/media/logo_b.1aba88ac.png"
+  }
+]);
